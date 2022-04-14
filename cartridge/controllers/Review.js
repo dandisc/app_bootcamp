651f8f760b6b8261a5c749f2a401b12951270c40
dandiscolo <daniele.discolo@skylabs.it>
2022-04-14 12:11:49 +0200

    First commit with new cartidge - Review

---
+++ cartridge/controllers/Review.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * @namespace Review
+ */
+
+var server = require('server');
+
+var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
+var userLoggedIn = require('*/cartridge/scripts/middleware/userLoggedIn');
+var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+
+/**
+ * Account-Show : The Account-Show endpoint will render the shopper's account page. Once a shopper logs in they will see is a dashboard that displays profile, address, payment and order information.
+ * @name Base/Review-Show
+ * @function
+ * @memberof Review
+ * @param {middleware} - server.middleware.https
+ * @param {middleware} - userLoggedIn.validateLoggedIn
+ * @param {middleware} - consentTracking.consent
+ * @param {querystringparameter} - registration - A flag determining whether or not this is a newly registered account
+ * @param {category} - senstive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
+server.get(
+    'Show',
+    server.middleware.https,
+    userLoggedIn.validateLoggedIn,
+    consentTracking.consent,
+    function (req, res, next) {
+        var CustomerMgr = require('dw/customer/CustomerMgr');
+        var Resource = require('dw/web/Resource');
+        var URLUtils = require('dw/web/URLUtils');
+        var accountHelpers = require('*/cartridge/scripts/account/accountHelpers');
+        var reportingUrlsHelper = require('*/cartridge/scripts/reportingUrls');
+        var reportingURLs;
+
+        res.render('account/review/addReview', {
+            breadcrumbs: [
+                {
+                    htmlValue: Resource.msg('global.home', 'common', null),
+                    url: URLUtils.home().toString()
+                },
+                {
+                    htmlValue: Resource.msg('label.review', 'account', null),
+                    url: '#'
+                }
+            ]
+        });
+        next();
+    }
+);
+
+
+module.exports = server.exports();
